bdeadf050a789885bdce8ac88b2600ec089d7af4
Huy Le Minh <leminhhuy.hcmus@gmail.com>
2021-11-09 00:49:54 +0700

    initial server

---
+++ server/src/controllers/index.js
@@ -0,0 +1,5 @@
+import InvoiceController from "./InvoiceController.js";
+
+//New controllers will be added to this object.
+const AppControllers = [new InvoiceController()];
+export default AppControllers;
